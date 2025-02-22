4ad762a669d0da86f766ffd2d6bb6a72eaa0227b
light <lights.darke@gmail.com>
2025-02-22 00:18:56 -0500

    claw tiny fix (guess)

--- TeamCode/src/main/java/Outtake.java
+++ TeamCode/src/main/java/Outtake.java
@@ -18,8 +18,8 @@ public class Outtake {
 
     public static double ArmPosition = 0;
     public static boolean clawOpen = false;
-    public static double CLAW_CLOSED = 0.523;
-    public static double CLAW_OPENED = 0.21;
+    public static double CLAW_CLOSED = 0.535;
+    public static double CLAW_OPENED = 0.215;
     //tune these values vvvvv
     public static double ARM_SAMPSCORE_POS = 0.75;
     public static double ARM_TRANSFER_POS = 0.14;
