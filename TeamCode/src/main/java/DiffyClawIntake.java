1b1a77d63fae53de099babe57ee4d26f897e28f8
light <lights.darke@gmail.com>
2025-02-28 20:30:16 -0500

    New Intake Coded :D

---
+++ TeamCode/src/main/java/DiffyClawIntake.java
@@ -0,0 +1,177 @@
+
+import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.opencv.features2d.AKAZE;
+
+@Config
+public class DiffyClawIntake {
+    private Servo IntakeClamp;
+    private Servo IntakeRDiffy;
+    private Servo IntakeLDiffy;
+    private Servo RightArmPivot;
+    private Servo LeftArmPivot;
+    public Servo leintake, reintake;
+
+    public static double DEFAULT_LDIFFY_POS = 0.00;
+    public static double DEFAULT_RDIFFY_POS = 0.22;
+    public static double LdiffyPos = DEFAULT_LDIFFY_POS;
+    public static double RdiffyPos = DEFAULT_RDIFFY_POS;
+
+    public static double ArmPosition = 0;
+    public static boolean clawOpen = false;
+    public static double CLAW_CLOSED = 0.535;
+    public static double CLAW_OPENED = 0.215;
+    //tune these values vvvvv
+    public static double ARM_REST = 0;
+    public static double ARM_TRANSFER_POS = 0;
+    public static double ARM_PICKUP_READY = 0;
+    public static double ARM_PICKUP_DOWN = 0;
+
+    /** LINKAGE EXTENSION VARIABLES */
+    public static double extPos = 0;
+    final double LINK1 = Math.sqrt(97408); // Length of first linkage (Linkage that connects to servo) (mm) (correct)
+    final double LINK2 = 320; // Length of second linkage (Linkage that connects to the slide) (mm) (correct)
+    final double XOFFSET = 97; // Offset X axis (CURRENT VALUE IS CORRECT)
+    final double YOFFSET = 8.25; // Offset Y axis (CURRENT VALUE IS CORRECT)
+    public static double FULL_EXTENSION = 530; // Length of the slides when fully extended (mm)
+    public static double EXTENSION_ZERO_OFFSET = -0.02; // Servo Zero Offset
+    final int SERVO_RANGE = 300; // Servo Range in degrees
+
+    //other extendo variables
+    double extensionWaitTime = 0;
+    public static double distance;
+
+    public static double INTAKE_DOWN_EXTENSION_LIMIT = 0;
+    public static double TRANSFER_EXTENSION_POS = 0;
+    ElapsedTime extensionTime;
+
+
+    public enum IntakeState {
+        TRANSFER,
+        INTAKE_ARM_READY,
+        INTAKE_ARM_PICKUP,
+        INTAKE_REST,
+
+    }
+    public DiffyClawIntake(HardwareMap hardwareMap) {
+        IntakeClamp = hardwareMap.get(Servo.class, "intakeClamp");
+        IntakeClamp.setDirection(Servo.Direction.REVERSE);
+        RightArmPivot = hardwareMap.get(Servo.class, "rpivhigh");
+        LeftArmPivot = hardwareMap.get(Servo.class, "lpivhigh");
+        RightArmPivot.setDirection(Servo.Direction.FORWARD);
+        LeftArmPivot.setDirection(Servo.Direction.REVERSE);
+        IntakeRDiffy = hardwareMap.get(Servo.class,"Rdiffy");
+        IntakeLDiffy = hardwareMap.get(Servo.class,"Ldiffy");
+        IntakeRDiffy.setDirection(Servo.Direction.REVERSE);
+        IntakeLDiffy.setDirection(Servo.Direction.FORWARD);
+        reintake = hardwareMap.get(Servo.class,"reintake");
+        leintake = hardwareMap.get(Servo.class, "leintake");
+        reintake.setDirection(Servo.Direction.REVERSE);
+
+        extensionTime = new ElapsedTime();
+        extensionTime.startTime();
+
+    }
+    public static double Aligned_Position = 211;
+
+    @Config
+    public static class DIFFY_POSITIONS {
+        public static double TRANSFER_POS = 90;
+        public static double INTAKE_POS = 90;
+
+        public static double ORIENTATION_UP = 0;
+        public static double ORIENTATION_DOWN = 211;
+        public static double ORIENTATION_ALIGNED = Aligned_Position;
+
+    }
+
+    private void setPivotPosition(double UpDownAngle, double Orientation){
+        double ServoRange = 300;
+        LdiffyPos = DEFAULT_LDIFFY_POS + UpDownAngle/ServoRange + Orientation*((double) 18/52)/ServoRange;
+        RdiffyPos = DEFAULT_RDIFFY_POS + UpDownAngle/ServoRange - Orientation*((double) 18/52)/ServoRange;
+    }
+
+    private void updatePivotPosition(){
+        if(IntakeLDiffy.getPosition() != LdiffyPos || IntakeRDiffy.getPosition() != RdiffyPos){
+            IntakeLDiffy.setPosition(LdiffyPos);
+            IntakeRDiffy.setPosition(RdiffyPos);
+        }
+    }
+    // Intake Extension
+    private double getServoAngleWithLength(double l1, double l2, double l3, double xo, double yo, int servoRange){
+        // All units are mm and degrees.
+        double beta = Math.toDegrees(Math.acos((Math.pow(l1, 2) - Math.pow(l2, 2) + Math.pow(xo + l3, 2) + Math.pow(yo, 2))/(2.0*l1*Math.sqrt(Math.pow(xo+l3, 2) + Math.pow(yo, 2)))));
+        double gamma = Math.toDegrees(Math.atan((xo+l3)/yo));
+        return (180.0 - beta - gamma)/servoRange;
+    }
+    private void extendTo(double length){
+        double targetPos = EXTENSION_ZERO_OFFSET + getServoAngleWithLength(LINK1, LINK2, length, XOFFSET, YOFFSET, SERVO_RANGE);
+        if(leintake.getPosition() != targetPos){
+            extensionWaitTime = Math.abs(targetPos - leintake.getPosition());
+            extensionTime.reset();
+
+            leintake.setPosition(targetPos);
+            reintake.setPosition(targetPos);
+        }
+    }
+    public boolean isExtensionBusy(){
+        return extensionTime.time() <= extensionWaitTime;
+    }
+    public void setExtensionTarget(double target){
+        if(target > FULL_EXTENSION) target = FULL_EXTENSION;
+        else if(target < 0) target = 0;
+        extPos = target;
+    }
+    public void TeleopExtend(double valueFromZeroToOne){
+        if(valueFromZeroToOne < 0) valueFromZeroToOne = 0;
+        else if(valueFromZeroToOne > 1) valueFromZeroToOne = 1;
+        setExtensionTarget(valueFromZeroToOne * FULL_EXTENSION);
+    }
+
+
+    IntakeState intakeState = IntakeState.TRANSFER;
+    public void intakeLoop(){
+        extendTo(extPos);
+        switch(intakeState){
+            case TRANSFER:
+                ArmPosition = ARM_TRANSFER_POS;
+                setPivotPosition(DIFFY_POSITIONS.TRANSFER_POS, DIFFY_POSITIONS.ORIENTATION_UP);
+                break;
+            case INTAKE_ARM_READY:
+                ArmPosition = ARM_PICKUP_READY;
+                setPivotPosition(DIFFY_POSITIONS.INTAKE_POS, DIFFY_POSITIONS.ORIENTATION_ALIGNED);
+                break;
+            case INTAKE_ARM_PICKUP:
+                ArmPosition = ARM_PICKUP_DOWN;
+                setPivotPosition(DIFFY_POSITIONS.INTAKE_POS, DIFFY_POSITIONS.ORIENTATION_ALIGNED);
+                break;
+            case INTAKE_REST:
+                ArmPosition = ARM_REST;
+                setPivotPosition(DIFFY_POSITIONS.TRANSFER_POS, DIFFY_POSITIONS.ORIENTATION_UP);
+                break;
+        }
+
+        updatePivotPosition();
+
+        if (ArmPosition != RightArmPivot.getPosition()){
+            RightArmPivot.setPosition(ArmPosition);
+            LeftArmPivot.setPosition(ArmPosition);
+        }
+        if ((IntakeClamp.getPosition()!=CLAW_CLOSED) && !clawOpen){
+            IntakeClamp.setPosition(CLAW_CLOSED);
+        } else if ((IntakeClamp.getPosition()!=CLAW_OPENED) && clawOpen){
+            IntakeClamp.setPosition(CLAW_OPENED);
+        }
+    }
+
+    public void setIntakeState(IntakeState intakeState){
+        this.intakeState = intakeState;
+    }
+
+    public void setClawOpen(boolean state){
+        clawOpen = state;
+    }
+}
---
+++ TeamCode/src/main/java/EC_TeleOp_DIFFYIntakeOnly.java
@@ -0,0 +1,140 @@
+
+
+import com.acmerobotics.dashboard.config.Config;
+import com.pedropathing.follower.Follower;
+import com.pedropathing.localization.Pose;
+import com.pedropathing.util.Constants;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import pedroPathing.constants.FConstants;
+import pedroPathing.constants.LConstants;
+
+
+@TeleOp(name = "Main TeleOp", group = "Real OpModes")
+@Config
+public class EC_TeleOp_DIFFYIntakeOnly extends OpMode {
+    private Follower follower;
+
+    private DiffyClawIntake diffyClawIntake;
+    private ElapsedTime elapsedTime, intakeSequenceTime, resetEncoderDelay;
+    private final Pose startPose = Storage.CurrentPose;
+    private double targetHeading = 180, headingError, headingCorrection;
+    int flip = 1;
+    int initfsm = 0;
+
+    @Override
+    public void init() {
+        Constants.setConstants(FConstants.class, LConstants.class);
+        follower = new Follower(hardwareMap);
+        follower.setStartingPose(startPose);
+        elapsedTime = new ElapsedTime();
+        intakeSequenceTime = new ElapsedTime();
+        resetEncoderDelay = new ElapsedTime();
+        diffyClawIntake = new DiffyClawIntake(hardwareMap);
+
+        intakeSequenceTime.startTime();
+        elapsedTime.startTime();
+        resetEncoderDelay.startTime();
+        initfsm = 1;
+    }
+
+    private ToggleButton teamColorButton = new ToggleButton(Storage.isRed);
+    private ToggleButton controlFlipButton = new ToggleButton(true);
+
+    @Override
+    public void init_loop(){
+        teamColorButton.input(gamepad1.dpad_up);
+        Storage.isRed = teamColorButton.getVal();
+        follower.update();
+        Storage.CurrentPose = follower.getPose();
+        telemetry.addLine("DO NOT TOUCH IF THIS IS REAL GAME, or make sure you dont misclick.");
+        telemetry.addData("Team Color:", Storage.isRed ? "Red" : "Blue");
+        telemetry.update();
+    }
+
+    @Override
+    public void start() {
+        follower.startTeleopDrive();
+        diffyClawIntake.setIntakeState(DiffyClawIntake.IntakeState.INTAKE_REST);
+
+    }
+    public enum INTAKE_SEQUENCE{
+        TRANSFER, READY, GRAB;
+        private static final INTAKE_SEQUENCE[] vals = values();
+        public INTAKE_SEQUENCE next(){
+            return vals[(this.ordinal() + 1) % vals.length];
+        }
+        public INTAKE_SEQUENCE prev(){
+            return vals[(this.ordinal() - 1 + vals.length) % vals.length];
+        }
+    }
+    INTAKE_SEQUENCE intakeSequence = INTAKE_SEQUENCE.TRANSFER;
+    private ToggleButton intakeSequenceNextButton = new ToggleButton(true), intakeSequencePreviousButton = new ToggleButton(true);
+    @Override
+    public void loop() {
+        if(intakeSequenceNextButton.input(gamepad1.left_bumper)){
+            intakeSequence = intakeSequence.next();
+            intakeSequenceTime.reset();
+        } else if(intakeSequencePreviousButton.input(gamepad1.right_bumper)){
+            intakeSequence = intakeSequence.prev();
+            intakeSequenceTime.reset();
+        }
+        switch (intakeSequence){
+            case READY:
+                diffyClawIntake.setIntakeState(DiffyClawIntake.IntakeState.INTAKE_ARM_READY);
+                diffyClawIntake.setExtensionTarget(DiffyClawIntake.FULL_EXTENSION);
+                if (!diffyClawIntake.isExtensionBusy()){
+                    diffyClawIntake.setClawOpen(true);
+                }
+                break;
+            case GRAB:
+                diffyClawIntake.setIntakeState(DiffyClawIntake.IntakeState.INTAKE_ARM_PICKUP);
+                diffyClawIntake.setExtensionTarget(DiffyClawIntake.FULL_EXTENSION);
+                if (intakeSequenceTime.time() > 0.2){
+                    diffyClawIntake.setClawOpen(false);
+                }
+                break;
+            case TRANSFER:
+                diffyClawIntake.setExtensionTarget(DiffyClawIntake.TRANSFER_EXTENSION_POS);
+                diffyClawIntake.setIntakeState(DiffyClawIntake.IntakeState.TRANSFER);
+                break;
+        }
+        diffyClawIntake.intakeLoop();
+
+        controlFlipButton.input(gamepad1.dpad_up);
+        flip = controlFlipButton.getVal() ? 1 : -1;
+
+
+        if (intakeSequence == INTAKE_SEQUENCE.TRANSFER) {
+            follower.setTeleOpMovementVectors(
+                    flip * 0.48 * Math.tan(1.12 * -gamepad1.left_stick_y),
+                    flip * 0.48 * Math.tan(1.12 * -gamepad1.left_stick_x),
+                    0.21 * Math.tan(1.12 * -gamepad1.right_stick_x), true);
+        } else { //if intake is down, then we slow down the driving.
+            follower.setTeleOpMovementVectors(
+                    flip * 0.44 * Math.tan(1.12 * -gamepad1.left_stick_y),
+                    flip * 0.44 * Math.tan(1.12 * -gamepad1.left_stick_x),
+                    0.15 * Math.tan(1.12 * -gamepad1.right_stick_x), true);
+
+        }
+        follower.update();
+        Storage.CurrentPose = follower.getPose();
+
+        telemetry.addData("Control:", controlFlipButton.getVal() ? "Normal" : "Flipped");
+        telemetry.addData("Target Heading in Degrees", Math.toDegrees(targetHeading));
+        telemetry.addData("Angle Error in Degrees", Math.toDegrees(headingError));
+        telemetry.addData("Correction Vector in Degrees", Math.toDegrees(headingCorrection));
+        telemetry.addData("X", follower.getPose().getX());
+        telemetry.addData("Y", follower.getPose().getY());
+        telemetry.addData("Heading in Degrees", Math.toDegrees(follower.getPose().getHeading()));
+        telemetry.addLine();
+        telemetry.addData("Elapsed Time", elapsedTime.toString());
+        telemetry.addLine();
+        telemetry.addData("Horizontal Extension Target Position", Intake.extPos);
+        telemetry.addData("Horizontal Extension Servo Angle", diffyClawIntake.leintake.getPosition());
+
+        telemetry.update();
+    }
+}
